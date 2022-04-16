b948a65dde6efff543617415177934d67a5d0386
Cisco <diasfrancisco7798@yahoo.co.uk>
2022-04-17 00:29:46 +0100

    made the main page mobile responsive

--- apply.js
+++ apply.js
@@ -8,3 +8,11 @@ window.onscroll = function() {
     }
     prevScrollpos = currScrollpos;
 }
+
+
+const toggleButton = document.getElementsByClassName('toggle-menu')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
