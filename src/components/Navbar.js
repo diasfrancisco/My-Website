351757b528ad1e6525a5c550bb803c1f127e22a2
CiscoUbuntu <diasfrancisco7798@yahoo.co.uk>
2022-10-18 18:03:41 +0100

    Uses React and Node js to recreate the website

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+
+function Navbar() {
+  return (
+    <div className='navbarContainer'>
+        <div className='navItems'>Logo</div>
+        <div className='navItems'>Links</div>
+    </div>
+  )
+}
+
+export default Navbar
