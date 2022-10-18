659ce72c287f9c8582c40b9996a983c53a489da4
CiscoUbuntu <diasfrancisco7798@yahoo.co.uk>
2022-10-18 23:41:51 +0100

    Adds some styling to the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,18 @@ function Navbar() {
     <div id='navbar'>
       <img src='' className='logo' alt='the main logo'></img>
       <div className='navbarContainer'>
-          <button className='navItem'>Newsletter</button>
-          <button className='navItem'>About</button>
-          <button className='navItem'>Contact</button>
-          <button className='navItem'>Affiliates</button>
+        <div>
+        <button className='navItem'>Newsletter</button>
+        </div>
+        <div>
+        <button className='navItem'>About</button>
+        </div>
+        <div>
+        <button className='navItem'>Contact</button>
+        </div>
+        <div>
+        <button className='navItem'>Affiliates</button>
+        </div>
       </div>
     </div>
   )
