a282aa20763459620d166a783fca04d1dd0a94fc
CiscoUbuntu <diasfrancisco7798@yahoo.co.uk>
2022-10-23 00:08:52 +0100

    Updates introductory message

--- src/components/IntroMessage/IntroMessage.jsx
+++ src/components/IntroMessage/IntroMessage.jsx
@@ -3,7 +3,11 @@ import './IntroMessage.css'
 
 const IntroMessage = () => {
   return (
-    <div className='intro__message'>IntroMessage</div>
+    <div className='intro__message-container'>
+      <div className='intro__message'>Hi</div>
+      <div className='intro__message'>I'm Francisco</div>
+      <div className='intro__message'>I enjoy Art, Computing and Science</div>
+    </div>
   )
 }
 
