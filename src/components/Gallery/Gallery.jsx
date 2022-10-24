9a225c6b67db604480c0ed310c4ef118cd6f49ca
CiscoUbuntu <diasfrancisco7798@yahoo.co.uk>
2022-10-24 22:53:06 +0100

    Adds the gallery image and centers it in the Introduction section

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Gallery.css'
+import itachi from '../../assets/images/itachi-main.png'
 
 const Gallery = () => {
   return (
-    <div className='gallery'>Gallery</div>
+    <div className='gallery'>
+      <img src={itachi} alt="Itachi from Naruto" />
+    </div>
   )
 }
 
